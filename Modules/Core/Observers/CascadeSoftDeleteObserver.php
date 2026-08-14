7c2b5cb9405a3b9c22153a0d3884a2528c03462a
eng-mhd-ammar <n.ammarnw6@gmail.com>
2026-08-14 16:21:59 +0300

    fix

--- Modules/Core/Observers/CascadeSoftDeleteObserver.php
+++ Modules/Core/Observers/CascadeSoftDeleteObserver.php
@@ -8,32 +8,60 @@ class CascadeSoftDeleteObserver
 {
     public function deleting(Model $model): void
     {
-        if (property_exists($model, 'cascadeDeletes')) {
-            foreach ($model->cascadeDeletes as $relation) {
-                foreach ($model->$relation as $related) {
-                    if (!is_null($related)) {
-                        $related->delete();
-                    }
+        if (!property_exists($model, 'cascadeDeletes')) {
+            return;
+        }
+
+        foreach ($model->cascadeDeletes as $relation) {
+            $relationInstance = $model->{$relation}();
+
+            if ($relationInstance instanceof HasOne) {
+                $related = $relationInstance->first();
+
+                if ($related) {
+                    $related->delete();
                 }
+
+                continue;
+            }
+
+            foreach ($relationInstance->get() as $related) {
+                $related->delete();
             }
         }
     }
 
     public function restoring(Model $model): void
     {
-        if (request()->input('restore_cascade', 0) == 1) {
-            if (property_exists($model, 'cascadeDeletes')) {
-                foreach ($model->cascadeDeletes as $relation) {
-                    $model->$relation()
-                            ->onlyTrashed()
-                            ->get()
-                            ->each(function ($related): void {
-                                if (!is_null($related)) {
-                                    $related->restore();
-                                }
-                            });
+        if (request()->input('restore_cascade', 0) != 1) {
+            return;
+        }
+
+        if (!property_exists($model, 'cascadeDeletes')) {
+            return;
+        }
+
+        foreach ($model->cascadeDeletes as $relation) {
+            $relationInstance = $model->{$relation}();
+
+            if ($relationInstance instanceof HasOne) {
+                $related = $relationInstance
+                    ->onlyTrashed()
+                    ->first();
+
+                if ($related) {
+                    $related->restore();
                 }
+
+                continue;
             }
+
+            $relationInstance
+                ->onlyTrashed()
+                ->get()
+                ->each(function ($related): void {
+                    $related->restore();
+                });
         }
     }
 }
